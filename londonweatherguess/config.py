f4e0c45618b1df37e841ca204894be473785468c
Daniella Omokore <daniellaomokore@gmail.com>
2023-04-11 14:23:17 +0100

    commit

--- londonweatherguess/config.py
+++ londonweatherguess/config.py
@@ -12,22 +12,20 @@ load_dotenv()
 # Mysqlworkbench
 # environmental variables stored
 
-"""DATABASENAME = "tempGuess_user"
-"""
-"""
+DATABASENAME = "tempGuess_user"
+
 USER = os.environ.get('USER')
 DATABASEPASSWORD = os.environ.get('DATABASEPASSWORD')  # THIS IS YOUR MYSQLWORKBENCH PASSWORD
 HOST = os.environ.get('HOST')
 
 # secret key
-SECRET_KEY = os.environ.get('SECRET_KEY')"""
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
-
-DATABASENAME = "heroku_b30aff198b29397"
+"""DATABASENAME = "heroku_b30aff198b29397"
 USER = "b03ee4d90b32be"
 DATABASEPASSWORD = "70cbe40f"  # THIS IS YOUR MYSQLWORKBENCH PASSWORD
 HOST = "us-cdbr-east-06.cleardb.net"
 
 # secret key
 SECRET_KEY = "secretkey"
-
+"""
